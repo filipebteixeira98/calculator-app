b4c39179f54d887c330d9c3090806590aa6628c5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-08-31 09:39:16 -0300

    style: add calculator color palette

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    background: '#000',
+    displayText: '#fff',
+    numberButton: '#333',
+    operatorButton: '#ff9f0a',
+    functionButton: '#a5a5a5',
+    functionText: '#000',
+    text: '#fff',
+}
