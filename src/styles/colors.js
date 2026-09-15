806b734736ed960ab1e9acb8609bab932db8fbcd
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-09-14 23:24:28 -0300

    refactor(styles): extend shared color tokens

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,9 +1,12 @@
 export const colors = {
     background: '#000',
+    surface: '#1c1c1e',
     displayText: '#fff',
     numberButton: '#333',
     operatorButton: '#ff9f0a',
     functionButton: '#a5a5a5',
     functionText: '#000',
     text: '#fff',
+    secondaryText: '#b5b5b5',
+    border: '#2c2c2e',
 }
