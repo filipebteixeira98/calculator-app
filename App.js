0b08b846789bb681430c8b1195dee45e9fa7a454
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-09-14 23:59:42 -0300

    feat(navigation): register converter screen

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { HomeScreen } from './src/screens/HomeScreen';
 import { CalculatorScreen } from './src/screens/CalculatorScreen';
+import { ConverterScreen } from './src/screens/ConverterScreen';
+
+import { colors } from './src/styles/colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,13 +14,14 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerStyle: { backgroundColor: '#000' },
-          headerTintColor: '#fff',
-          contentStyle: { backgroundColor: '#000' },
+          headerStyle: { backgroundColor: colors.background },
+          headerTintColor: colors.text,
+          contentStyle: { backgroundColor: colors.background },
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
         <Stack.Screen name="Calculator" component={CalculatorScreen} options={{ title: 'Calculator' }} />
+        <Stack.Screen name="Converter" component={ConverterScreen} options={{ title: 'Converter' }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
